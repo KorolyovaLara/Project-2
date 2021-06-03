46735a49ca13cf238391d768b97f0b87488ccdcb
Lara Coding <78417151+JoyofCodingLife@users.noreply.github.com>
2021-06-03 20:32:06 +1000

    seeds for users, games and collection

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE gg_db;
 
 CREATE TABLE users (
 	userId INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  firstName VARCHAR(30) DEFAULT NULL,
+	firstName VARCHAR(30) DEFAULT NULL,
 	lastName VARCHAR(30) DEFAULT NULL,
 	username VARCHAR(30) NOT NULL, 
 	email VARCHAR(50) NOT NULL,
@@ -17,64 +17,64 @@ CREATE TABLE users (
 
 CREATE TABLE games (
 	gameId INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  game_title VARCHAR(30) NOT NULL,
-  users_playing INTEGER
+	game_title VARCHAR(30) NOT NULL,
+	users_playing INTEGER
 );
     
 CREATE TABLE game_collection (
-  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  gameId INTEGER NOT NULL  REFERENCES games(gameId),
-  userId INTEGER NOT NULL REFERENCES users(userId),
+	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	gameId INTEGER NOT NULL  REFERENCES games(gameId),
+	userId INTEGER NOT NULL REFERENCES users(userId)
 );
     
 CREATE TABLE post (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  authorId INTEGER NOT NULL,
-  parentId INTEGER DEFAULT NULL,
-  title VARCHAR(75) NOT NULL,
-  slug VARCHAR(100) NOT NULL,
-  summary TINYTEXT,
-  createdAt DATETIME NOT NULL,
-  updatedAt DATETIME DEFAULT NULL,
-  publishedAt DATETIME DEFAULT NULL,
-  content TEXT  ,
-  PRIMARY KEY (id),
-  UNIQUE KEY uq_slug (slug),
-  KEY idx_post_user (authorId),
-  KEY idx_post_parent (parentId),
-  CONSTRAINT fk_post_parent FOREIGN KEY (parentId) REFERENCES post (id),
-  CONSTRAINT fk_post_user FOREIGN KEY (authorId) REFERENCES users (userId)
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	authorId INTEGER NOT NULL,
+	parentId INTEGER DEFAULT NULL,
+	title VARCHAR(75) NOT NULL,
+	slug VARCHAR(100) NOT NULL,
+	summary TINYTEXT,
+	createdAt DATETIME NOT NULL,
+	updatedAt DATETIME DEFAULT NULL,
+	publishedAt DATETIME DEFAULT NULL,
+	content TEXT  ,
+	PRIMARY KEY (id),
+	UNIQUE KEY uq_slug (slug),
+	KEY idx_post_user (authorId),
+	KEY idx_post_parent (parentId),
+	CONSTRAINT fk_post_parent FOREIGN KEY (parentId) REFERENCES post (id),
+	CONSTRAINT fk_post_user FOREIGN KEY (authorId) REFERENCES users (userId)
 ); 
 
 CREATE TABLE post_comment (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  postId INTEGER NOT NULL,
-  parentId INTEGER DEFAULT NULL,
-  title VARCHAR(100) NOT NULL,
-  createdAt DATETIME NOT NULL,
-  publishedAt DATETIME DEFAULT NULL,
-  content TEXT,
-  PRIMARY KEY (id),
-  KEY idx_comment_post (postId),
-  KEY idx_comment_parent (parentId),
-  CONSTRAINT fk_comment_parent FOREIGN KEY (parentId) REFERENCES post_comment (id),
-  CONSTRAINT fk_comment_post FOREIGN KEY (postId) REFERENCES post (id)
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	postId INTEGER NOT NULL,
+	parentId INTEGER DEFAULT NULL,
+	title VARCHAR(100) NOT NULL,
+	createdAt DATETIME NOT NULL,
+	publishedAt DATETIME DEFAULT NULL,
+	content TEXT,
+	PRIMARY KEY (id),
+	KEY idx_comment_post (postId),
+	KEY idx_comment_parent (parentId),
+	CONSTRAINT fk_comment_parent FOREIGN KEY (parentId) REFERENCES post_comment (id),
+	CONSTRAINT fk_comment_post FOREIGN KEY (postId) REFERENCES post (id)
 ); 
 
 CREATE TABLE tag (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  title VARCHAR(75) NOT NULL,
-  slug VARCHAR(100) NOT NULL,
-  content TEXT,
-  PRIMARY KEY (id)
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	title VARCHAR(75) NOT NULL,
+	slug VARCHAR(100) NOT NULL,
+	content TEXT,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE post_tag (
-  postId INTEGER NOT NULL,
-  tagId INTEGER NOT NULL,
-  PRIMARY KEY (postId, tagId),
-  KEY idx_pt_tag (tagId),
-  KEY idx_pt_post (postId),
-  CONSTRAINT fk_pt_post FOREIGN KEY (postId) REFERENCES post (id),
-  CONSTRAINT fk_pt_tag FOREIGN KEY (tagId) REFERENCES tag (id)
+	postId INTEGER NOT NULL,
+	tagId INTEGER NOT NULL,
+	PRIMARY KEY (postId, tagId),
+	KEY idx_pt_tag (tagId),
+	KEY idx_pt_post (postId),
+	CONSTRAINT fk_pt_post FOREIGN KEY (postId) REFERENCES post (id),
+	CONSTRAINT fk_pt_tag FOREIGN KEY (tagId) REFERENCES tag (id)
 );
