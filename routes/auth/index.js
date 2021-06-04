a724050542ff14be9175c53e4afb017082130690
Tinku Bansal <tinkubansal21@gmail.com>
2021-06-04 23:44:03 +1000

    Added logout route

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const authRoutes = require("./auth");
+const homeRoutes = require("./homeRoutes");
 
 router.use("/auth", authRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
