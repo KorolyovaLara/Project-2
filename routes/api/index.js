c87f9b342e67ae20b90bf3b726f5b787bbdda486
Tinku Bansal <tinkubansal21@gmail.com>
2021-06-05 12:51:07 +1000

    Added functionality to create new tags

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const gameRoutes = require("./gameRoutes");
+const tagRoutes = require("./tagRoutes");
 
 router.use("/games", gameRoutes);
+router.use("/tags", tagRoutes);
 
 module.exports = router;
