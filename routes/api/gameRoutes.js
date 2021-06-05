1711f8484a37c9b0732609b25351abc01493bed9
Tinku Bansal <tinkubansal21@gmail.com>
2021-06-05 21:53:13 +1000

    Resolved issues

--- routes/api/gameRoutes.js
+++ routes/api/gameRoutes.js
@@ -7,11 +7,7 @@ const router = require("express").Router();
 router.post("/", async (req, res) => {
   const { gameTitle } = req.body;
 
-  if (
-    ![gameTitle].every(
-      (item) => item.trim() !== ""
-    )
-  ) {
+  if ( ! (gameTitle.trim() !== "" )) {
     res.status(404).send({ message: "Invalid parameters" });
     return;
   }
