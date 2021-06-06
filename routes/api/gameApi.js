b62a602ac78512c3688b01c75b0138b2c7b080c8
Lara Coding <78417151+JoyofCodingLife@users.noreply.github.com>
2021-06-07 08:51:52 +1000

    added - find all games for user

--- routes/api/gameApi.js
+++ routes/api/gameApi.js
@@ -1,11 +1,29 @@
 const router = require("express").Router();
 
 const Games = require("../../models/Games");
+const Users = require("../../models/Users");
+const GameCollection = require("../../models/GameCollection");
 const { withAuth } = require("../../utils/auth");
 
 // find all games for the user
-router.get("/", withAuth, async (req, res) => {});
+router.get("/id", withAuth, async (req, res) => {
+  try {
+    const userGames = await Games.findByPk(req.params.id, {
+      include: [{ model: Users, through: GameCollection, as: "user_games" }],
+    });
 
+    if (!userGames) {
+      res.status(404).json({ message: "No collection found for this user!" });
+      return;
+    }
+
+    res.status(200).json(userGames);
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+// create a new game
 router.post("/", withAuth, async (req, res) => {
   try {
     const user = req.user;
@@ -19,13 +37,13 @@ router.post("/", withAuth, async (req, res) => {
       });
 
       res.json(game);
-    } catch (e) {
-      console.log(e);
+    } catch (err) {
+      console.log(err);
       return res
         .status(409)
         .json({ message: `The title "${title}" already exists.` });
     }
-  } catch (e) {
+  } catch (err) {
     res.status(500).json({ message: "Something went wrong" });
   }
 });
