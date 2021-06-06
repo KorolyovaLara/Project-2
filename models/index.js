8f877b3eab6645e29ce2c864e43991674771a4b2
Strahinja Ajvaz <strahinja.ajvaz@gmail.com>
2021-06-06 17:36:56 +1000

    removed unnecessary files

--- models/index.js
+++ models/index.js
@@ -2,15 +2,9 @@
 const Users = require("./Users");
 const Games = require("./Games");
 const GameCollection = require("./GameCollection");
-const Post = require("./Post");
-const Comment = require("./Comment");
-const Tag = require("./Tag");
 
 // Model association imports
 const GameCollectionToGames = require("./relationalModels/GameCollectionToGames");
-const PostToTags = require("./relationalModels/PostsToTags");
-const PostToComments = require("./relationalModels/PostToComments");
-const UserToPosts = require("./relationalModels/UserToPosts");
 
 // user to game collection
 Users.hasOne(GameCollection);
@@ -23,25 +17,3 @@ GameCollection.hasMany(Games, { through: GameCollectionToGames });
 Games.belongsToMany(GameCollection, {
   through: GameCollectionToGames,
 });
-
-// user to post
-Users.hasMany(Post, { through: UserToPosts });
-Post.belongsToMany(Users, {
-  through: UserToPosts,
-});
-
-// user to comment
-Users.hasMany(Comment);
-Comment.belongsTo(Users, {
-  foreignKey: "authorId",
-});
-
-// post to comment
-Post.hasMany(Comment, { through: PostToComments });
-Comment.belongsTo(Post, {
-  through: PostToComments,
-});
-
-// post to tag
-Post.hasMany(Tag, { through: PostToTags });
-Tag.belongsToMany(Post, { through: PostToTags });
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
-const tagRoutes = require("./tagRoutes");
 const gameApi = require("./gameApi");
 
-router.use("/tags", tagRoutes);
 router.use("/api/game", gameApi);
 
 module.exports = router;
