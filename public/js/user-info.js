f383b566db8fdca2e8e479c6fb1ed0372da6f25f
Tinku Bansal <tinkubansal21@gmail.com>
2021-06-10 15:46:22 +1000

    added api to add existing game to user collection

--- public/js/user-info.js
+++ public/js/user-info.js
@@ -1,9 +1,12 @@
-var modal = document.querySelectorAll("#modal");
+var modalNewGame = document.querySelectorAll("#modal-new-game");
+var modalAddToCollection = document.querySelectorAll("#modal-game-collection");
 const gameTitle = $("#gameTitle");
 const gameDescription = $("#gameDescription");
 const gameTrailer = $("#gameTrailer");
-const form = $("form");
+const form = $("#addGame");
+const formGameCollection = $("#addToCollection");
 
+// controls for create new game modal
 $("#new-game-modal-button").click(function () {
   var target = $(this).data("target");
   $("html").addClass("is-clipped");
@@ -12,7 +15,19 @@ $("#new-game-modal-button").click(function () {
 
 $("#modal-close").click(function () {
   $("html").removeClass("is-clipped");
-  modal[0].classList.remove("is-active");
+  modalNewGame[0].classList.remove("is-active");
+});
+
+// controls for add game to collection modal
+$("#game-collection-button").click(function () {
+  var target = $(this).data("target");
+  $("html").addClass("is-clipped");
+  $(target).addClass("is-active");
+});
+
+$("#modal-close-user-collection").click(function () {
+  $("html").removeClass("is-clipped");
+  modalAddToCollection[0].classList.remove("is-active");
 });
 
 const closeNotification = $("#close_error_banner");
@@ -75,4 +90,25 @@ const handleSubmit = async (e) => {
   }
 };
 
+const handleSubmitGameCollection = async (e) => {
+  e.preventDefault();
+
+  const title = gameTitleOption.value;
+  const res = await fetch("/api/game/gameCollection", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+    }),
+  });
+  if (res.ok) {
+    location.reload();
+    return;
+  }
+};
+
 form.on("submit", handleSubmit);
+formGameCollection.on("submit", handleSubmitGameCollection);
